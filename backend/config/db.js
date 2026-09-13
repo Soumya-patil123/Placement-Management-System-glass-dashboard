1214a201cd703e52656804cccdb8644fa79cb562
Soumya-patil123 <soumyaspatilpatil34@gmail.com>
2026-09-13 19:45:29 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    mongoose.connect(process.env.MONGODB_URI)
-    console.log("MongoDB Connected");
+    await mongoose.connect("mongodb://127.0.0.1:27017/placementDB");
+    console.log("MongoDB Connected Successfully");
   } catch (error) {
-    console.error(error);
+    console.log(error);
     process.exit(1);
   }
 };
